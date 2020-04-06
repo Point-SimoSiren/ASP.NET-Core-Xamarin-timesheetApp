16a5ab6564348d5b12b3232e6a416f0d954cfd81
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2020-04-07 00:08:33 +0300

    Added xamarin code

---
+++ XamarinCodes/EmployeePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace XamarinTimesheet
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            employeeList.ItemsSource = new string[] { "Janne", "Hanna", "Raimo" };
+            
+        }
+
+
+        private async void LoadEmployees(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://timesheetrestapi.azurewebsites.net/");
+            string json = await client.GetStringAsync("/api/employees/");
+            string[] employees = JsonConvert.DeserializeObject<string[]>(json);
+
+            employeeList.ItemsSource = employees;
+
+        }
+    }
+}
