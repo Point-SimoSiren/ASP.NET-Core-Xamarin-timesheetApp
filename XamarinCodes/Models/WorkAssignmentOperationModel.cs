020a529170ba5d4e7a65448300e72b805e6b9443
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2020-05-06 19:23:31 +0300

    a

---
+++ XamarinCodes/Models/WorkAssignmentOperationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTimesheet.Models
+{
+    public class WorkAssignmentOperationModel
+    {
+        public string Operation { get; set;  }
+        public  string AssignmentTitle { get; set; }
+    }
+}
--- XamarinCodes/WorkAssignmentPage.xaml.cs
+++ XamarinCodes/WorkAssignmentPage.xaml.cs
@@ -8,24 +8,66 @@ using Xamarin.Forms;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Xamarin.Forms.Xaml;
+using XamarinTimesheet.Models;
+using Xamarin.Essentials; // Tämä tarvitaan koordinaatteja varten
 
 namespace XamarinTimesheet
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
     public partial class WorkAssignmentPage : ContentPage
     {
+
         public WorkAssignmentPage()
         {
             InitializeComponent();
-            assignmentList.ItemsSource = new string[] { " " };
+
+            assignmentList.ItemsSource = new string[] { "" };
+
         }
+
+
+        //------------------------------------------EVENT----LOAD-WORKS----------------------------------------------
 
         private async void LoadWorkAssignments(object sender, EventArgs e)
         {
 
-            assignmentList.ItemsSource = new string[] { "Ladataan...", "Loading... "};
+            //Alustus, ennen palvelimen vastauksen saapumista näytetään ilmoitus lataamisesta.
+            assignmentList.ItemsSource = new string[] { "Ladataan..." };
 
+
+            // -------------------Sijainnin haku ja näyttäminen--------
             try
+            {
+                
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                   
+                    longitudeLabel.Text = location.Longitude.ToString();
+
+                    latitudeLabel.Text = location.Latitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                longitudeLabel.Text = "error";
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                longitudeLabel.Text = "error";
+            }
+            catch (PermissionException pEx)
+            {
+                longitudeLabel.Text = "error";
+            }
+            catch (Exception ex)
+            {
+                longitudeLabel.Text = "error";
+            }
+
+            try //----------HTTP pyyntö--------------------
             {
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri("https://timesheetrestapi.azurewebsites.net/");
@@ -34,10 +76,139 @@ namespace XamarinTimesheet
 
                 assignmentList.ItemsSource = assignments;
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 string errorMessage = ex.GetType().Name + ": " + ex.Message;
                 assignmentList.ItemsSource = new string[] { errorMessage };
+            }
+        }
+
+        //--------------------------------------EVENT----START-WORK---------------------------------------------
+
+        private async void StartWork(object sender, EventArgs e)
+        {
+            string assignmentName = assignmentList.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(assignmentName))
+            {
+                await DisplayAlert("Työ puuttuu", "Valitse ensin aloitettava työtehtävä!", "OK");
+            }
+            else
+            {
+                try
+                {
+
+                    // Käytetään Xamarin sovellukseen luotua model luokkaa ja perustetaan objekti palvelimelle lähetettäväksi.
+
+                    WorkAssignmentOperationModel data = new WorkAssignmentOperationModel()
+                    {
+                        Operation = "Start",
+                        AssignmentTitle = assignmentName
+                    };
+
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri("https://timesheetrestapi.azurewebsites.net/");
+
+
+                    // Muutetaan em. data objekti Jsoniksi
+                    string input = JsonConvert.SerializeObject(data);
+                    StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+
+                    // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                    HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+
+                    // Otetaan vastaan palvelimen vastaus
+                    string reply = await message.Content.ReadAsStringAsync();
+
+
+                    //Asetetaan vastaus serialisoituna success muuttujaan
+                    bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+
+                    if (success)  // Näytetään ehdollisesti alert viesti
+                    {
+
+                        await DisplayAlert("Työn aloitus", "Työ aloitettu.", "Sulje"); // (otsikko, teksti, kuittausnapin teksti)
+                    }
+                    else
+                    {
+                        await DisplayAlert("Aloitus ei onnistu", "Työ on jo käynnissä!", "Sulje"); // Muutettu 4.5.
+                    }
+                }
+
+                catch (Exception ex) // Otetaan poikkeus ex muuttujaan ja sijoitetaan errorMessageen
+                {
+
+                    string errorMessage = ex.GetType().Name + ": " + ex.Message; // Poikkeuksen customoitu selvittäminen ja...
+
+                    assignmentList.ItemsSource = new string[] { errorMessage }; // ..näyttäminen list viewissä
+                }
+            }
+        }
+
+        //-----------------------------EVENT-----STOP-WORK------------------------------------------------------
+
+        private async void StopWork(object sender, EventArgs e)
+        {
+            {
+                string assignmentName = assignmentList.SelectedItem?.ToString();
+                if (string.IsNullOrEmpty(assignmentName))
+                {
+                    await DisplayAlert("Työ puuttuu", "Valitse ensin lopetettava työtehtävä!", "OK");
+                }
+                else
+                {
+                    try
+                    {
+
+                        // Käytetään Xamarin sovellukseen luotua model luokkaa ja perustetaan objekti.
+
+                        WorkAssignmentOperationModel data = new WorkAssignmentOperationModel()
+                        {
+                            Operation = "Stop",
+                            AssignmentTitle = assignmentName
+                        };
+
+                        HttpClient client = new HttpClient();
+                        client.BaseAddress = new Uri("https://timesheetrestapi.azurewebsites.net/");
+
+                        // Muutetaan em. objekti Jsoniksi
+
+                        string input = JsonConvert.SerializeObject(data);
+                        StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+                        // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                        HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+                        // Otetaan vastaan palvelimen vastaus
+                        string reply = await message.Content.ReadAsStringAsync();
+
+                        //Asetetaan vastaus serialisoituna success muuttujaan
+                        bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+
+                        if (success)  // Näytetään ehdollisesti alert viesti
+                        {
+
+                            await DisplayAlert("Työn lopetus", "Työ lopetettu.", "Sulje"); // (otsikko, teksti, kuittausnapin teksti)
+
+                            assignmentList.ItemsSource = ""; // Muutettu 4.5.
+                        }
+                        else
+                        {
+                            await DisplayAlert("Työn lopetus", "Työtä ei voitu lopettaa koska sitä ei ole aloitettu.", "Sulje"); // Muutettu 4.5.
+                        }
+                    }
+
+                    catch (Exception ex) // Otetaan poikkeus ex muuttujaan ja sijoitetaan errorMessageen
+                    {
+
+                        string errorMessage = ex.GetType().Name + ": " + ex.Message; // Poikkeuksen customoitu selvittäminen ja...
+
+                        assignmentList.ItemsSource = new string[] { errorMessage }; // ..näyttäminen list viewissä
+                    }
+                }
             }
         }
     }
